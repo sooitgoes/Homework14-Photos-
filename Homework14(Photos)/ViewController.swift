2c7370dd55023a84ddd7b135cf1066d28250bd45
Mikhail Laty <laty.mischa@gmail.com>
2023-07-16 13:17:00 +0300

    Initial Commit

---
+++ Homework14(Photos)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework14(Photos)
+//
+//  Created by Михаил Латий on 16.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
