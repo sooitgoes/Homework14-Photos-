270266de726b3b8bc41fd54a0e4dd42b7acf3bc0
Mikhail Laty <laty.mischa@gmail.com>
2023-07-20 13:38:28 +0300

    Add collectionView and configure background.

--- Homework14(Photos)/Sources/ViewControllers/AlbumViewController.swift
+++ Homework14(Photos)/Sources/ViewControllers/AlbumViewController.swift
@@ -9,8 +9,43 @@ import UIKit
 
 class AlbumViewController: UIViewController {
 
+    // MARK: - UI Elements
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewLayout()
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        return collection
+    }()
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Albums"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.backgroundColor = .white
+        setupHierarchy()
+        setupLayout()
     }
+
+    // MARK: - Setup
+    private func setupHierarchy() {
+        view.addSubview(collectionView)
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+
+    private func registerCellForCollection() {
+
+    }
+
+    // MARK: - CollectionViewLayout
+
 }
 
